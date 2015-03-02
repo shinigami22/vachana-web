4106f5c7c047ea0fc2c806609e09edc5ca61d7ce
devudilip <devu.dilip@gmail.com>
2015-03-03 01:55:38 +0530

    rescue block added for calling api module

--- lib/modules/http.rb
+++ lib/modules/http.rb
@@ -2,12 +2,16 @@ require 'rest_client'
 
 module HTTP
 
-	VACHANA_API = Rails.env == 'production' ? "http://vachana.sanchaya.net" : 'http://localhost:3005/'
+  VACHANA_API = Rails.env == 'production' ? "http://vachana.sanchaya.net" : 'http://localhost:3005/'
 
-	def self.get url
-		url =  "#{VACHANA_API}/#{url}" 
-		response = RestClient.get url
-	end
+  def self.get url
+    url =  "#{VACHANA_API}/#{url}"
+    begin
+      response = RestClient.get url
+    rescue Exception => e
+     "Please check your API URL #{VACHANA_API}"
+   end 
+ end
 
 
-end	
+end 
